8b7138a67f7291e449a1065ddcb25bbc77079deb
phinic925 <phinicnyangate480@gmail.com>
2022-07-14 16:16:58 +0100

    Done

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,16 @@
 // Code EyesOnMe Component Here
+import React from "react";
+function EyesOnMe(){
+    function eyesOnMeBlur(){
+        console.log("Hey! Eyes on me!")
+    }
+    function eyesOnMeFocus(){
+        console.log("Good!")
+    }
+    return(
+        <div>
+            <button onBlur={eyesOnMeBlur} onFocus={eyesOnMeFocus}> Eyes on me </button>
+        </div>
+    )
+}
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,14 @@
 // Code Keypad Component Here
+import React from "react";
+
+function Keypad(){
+    function keypad(){
+        console.log("Entering password...")
+    }
+    return(
+        <div>
+            <input type = "password" onChange={keypad}/>
+        </div>
+    )
+}
+export default Keypad;
